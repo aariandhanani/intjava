76576bd0e42113de9def02e25c0703524fbc2b63
aariandhanani <47797960+aariandhanani@users.noreply.github.com>
2019-05-01 15:54:42 -0500

    Add files via upload

--- BooleanArrayTest2.java
+++ BooleanArrayTest2.java
@@ -1,20 +1,34 @@
 package com.suarez;
 
 public class BooleanArrayTest2 {
-    public static void main(String[]args)
+    public static boolean[][] makeGrid(int rows, int cols)
     {
-        boolean firstarray[][] = {{true,false,true,false},{false,true,false,true},{true,false,true,false}};
-        display(firstarray);
-    }
-    public static void display(boolean x[][])
-    {
-        for (int row = 0; row<x.length;row++)
+        int number = 1;
+        boolean grid[][] = new boolean[rows][cols];
+        for (int tempRow = 0; tempRow < rows; tempRow++)
         {
-            for (int column = 0; column<x[row].length;column++)
-            {
-                System.out.print(x[row][column]+"\t");
+            for (int tempColumn = 0; tempColumn < cols; tempColumn++) {
+                if (number % 2 == 1)
+                {
+                    grid[tempRow][tempColumn] = true;
+                    number = 0;
+                }
+                else
+                {
+                    grid[tempRow][tempColumn] = false;
+                    number = 1;
+                }
+                System.out.print(" " + grid[tempRow][tempColumn]);
             }
-            System.out.println();
+            System.out.println(" ");
         }
+        return grid;
+
     }
+
+    //Just for testing:
+//    public static void main(String [] args)
+//    {
+//        makeGrid(2,5);
+//    }
 }
